88ab4e5c2b39ecac5543ab231d2ca8a56ad86d26
MathiasBenmansour <mathias70250@gmail.com>
2022-08-10 21:51:52 +0200

    modify-homePage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,6 @@ import "./style.css";
 import { useState } from "react";
 import News from "../News/News";
 
-
 export default function Home() {
   const [trueOver, setTrueOver] = useState(false);
 
@@ -13,26 +12,27 @@ export default function Home() {
     setTrueOver(!trueOver);
   };
   return (
-    <div className="container">
-    
-        <article className="container-news">
-        <News />
-      </article>
+    <>
+      <div className="container-home">
+        <h1 className="welcome">
+         Le Portfolio est en cours de construction merci de patienter...
+        </h1>
+      </div>
+      <footer className="footer-cv">
+        <img className="img-robot" src={robot} alt="robot" />
+        <h2 className="introduction--description">
+          Hello !! , My name is Mathias and I am a junior web developer. I
+          present you today my first one which will gather all my future
+          projects.
+          <button className="document--cv">
+            <a onMouseOver={handleHover} href={cv} download="cv-Mathias">
+              <i className="title-cv"> Download CV</i>
 
-<footer className="footer-cv">
-      <img className="img-robot" src={robot} alt="robot" />
-      <h2 className="introduction--description">
-        Hello !! , My name is Mathias and I am a junior web developer. I present
-        you today my first one which will gather all my future projects.
-        <button className="document--cv">
-          <a onMouseOver={handleHover} href={cv} download="cv-Mathias">
-            <i className="title-cv"> Download CV</i>
-
-            {trueOver ? <h1 className="tooltip">Cv Mathias</h1> : ""}
-          </a>
-        </button>
-      </h2>
+              {trueOver ? <h1 className="tooltip">Cv Mathias</h1> : ""}
+            </a>
+          </button>
+        </h2>
       </footer>
-    </div>
+    </>
   );
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,40 +1,18 @@
 import "./style.css";
-import Search from "../Search/Search";
-import Footer from "../Footer/Footer";
+import { useState } from "react";
+import News from "../News/News";
 
-export default function Projects() {
+export default function Home() {
+  const [trueOver, setTrueOver] = useState(false);
+
+  const handleHover = () => {
+    setTrueOver(!trueOver);
+  };
   return (
-    <div className="container-search">
-      <div className="container-project">
-        <h1 className="title--project">List of projects</h1>
-        <div className="project-intro">
-          <Search />
-          <button className="Idle-rpg">
-            <a target="_blank" href=" http://idle-rpg.surge.sh/">
-              Idle
-            </a>
-          </button>
-
-          <button className="btn-morpion">
-            <a target="_blank" href="https://morpion-game.surge.sh/">
-              Morpion
-            </a>
-          </button>
-
-          <button className="btn-dofous">
-            <a target="_blank" href="https://dofous-kamous.surge.sh/">
-              Kamas
-            </a>
-          </button>
-          <button className="back-home">
-            <a className="link-home" href="/">
-              Home
-            </a>
-          </button>
-        </div>
-      </div>
-
-      <Footer />
+    <div className="container">
+      <article className="container-news">
+        <News />
+      </article>
     </div>
   );
 }
